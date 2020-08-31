4ecadd338a7424b5013263eaf5aa95e86bf5e1fb
Rahul-Rdr <rahul.char05@gmail.com>
2020-09-01 03:34:22 +0530

    new-selenium 4.0

--- Rise-Introduction/src/SelWebdriver/GetTitle.java
+++ Rise-Introduction/src/SelWebdriver/GetTitle.java
@@ -7,7 +7,7 @@ public class GetTitle {
 	
 	public static void main (String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Rahul-Official\\\\Documents\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "F:\\SELENIUM\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
--- Rise-Introduction/src/Testt.java
+++ Rise-Introduction/src/Testt.java
@@ -3,30 +3,24 @@ import java.time.LocalTime;
 public class Testt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		//LocalTime now = LocalTime.now();
+		LocalTime startTime = LocalTime.of(01, 00, 00, 000);
+		LocalTime endTime = LocalTime.of(06, 30, 00, 000);
 
-		// TODO Please Specify the required time below (Hour , Minute , Second ,
-		// NanoSecond)
-		LocalTime startTime = LocalTime.of(01, 30, 00, 000);
-		LocalTime endTime = LocalTime.of(01, 59, 00, 000);
-
-		System.out.println("Specific Time of Day=" + startTime);
-	
 
 		Testt dateTimeExample = new Testt();
 		try {
 			dateTimeExample.execute(startTime, endTime);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (InterruptedException e) {
+			
 			e.printStackTrace();
 		}
 
-		System.out.println("End of main");
+		System.out.println("Interval finish");
 	}
 	private void execute(LocalTime startTime, LocalTime endTime) throws InterruptedException {
-		Boolean isOn = false;
+		Boolean acIsOn = false;
 		LocalTime now = null;
 
 		while (true) {
@@ -34,19 +28,20 @@ public class Testt {
 			now = LocalTime.now();
 
 			if (now.isAfter(endTime)) {
-				System.out.println("Exiting from loop..");
+				System.out.println("Enter future time on endTime");
 				break;
 			}
 
-			if (now.isAfter(startTime) && !isOn) {
+			if (now.isAfter(startTime) && !acIsOn) {
 				System.out.println("Turning on AC....");
-				isOn = true;
-				Thread.sleep(2000);
+				acIsOn = true;
+				Thread.sleep(54000); //this is mili sec
 				
-			} else if (now.isAfter(startTime) && isOn) {
+				
+			} else if (now.isAfter(startTime) && acIsOn) {
 				System.out.println("Turning off AC....");
-				isOn = false;
-				Thread.sleep(2000);
+				acIsOn = false;
+				Thread.sleep(72000); //this is mili sec
 			}
 
 		}
